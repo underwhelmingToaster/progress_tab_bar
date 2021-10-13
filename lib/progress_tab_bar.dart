d5b4b158d1110b044b25cb37a90ff9657c03f320
Fabio Zahner <zahnerfabio@gmail.com>
2021-10-13 10:39:40 +0200

    adjustments to imports

--- lib/progress_tab_bar.dart
+++ lib/progress_tab_bar.dart
@@ -1,6 +1,8 @@
 library progress_tab_bar;
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:progress_tab_bar/src/painter.dart';
 
 class ProgressTabBar extends StatefulWidget {
   const ProgressTabBar({Key? key}) : super(key: key);
@@ -18,17 +20,25 @@ class _ProgressTabBarState extends State<ProgressTabBar> {
 }
 
 class ProgressTab extends StatefulWidget {
+  const ProgressTab({Key? key}) : super(key: key);
+
   @override
-  _ProgressTabBarState createState() => _ProgressTabBarState();
+  _ProgressTabState createState() => _ProgressTabState();
 
 }
 
 class _ProgressTabState extends State<ProgressTab> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+    double size = 200;
+    return RawMaterialButton(
+      child: CustomPaint(
+        size: Size(size,(size*0.3076923076923077).toDouble()),
+        painter: ButtonPainter(),
+      ),
+      onPressed: () { },
 
+    );
+  }
 }
 
